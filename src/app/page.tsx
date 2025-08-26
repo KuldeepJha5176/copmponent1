e4a18d564dc49d10130fddbe2d7596e15d14ce1e
KuldeepJha5176 <ranacjha@gmail.com>
2025-08-26 23:41:32 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <div className=" absolute inset-0 z-0 rounded-lg 
       mx-auto bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] 
       bg-[size:10px_10px] bg-fixed"></div>
+      hi there
     </div>
   );
 }
